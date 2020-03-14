5f0fdc6da02625c6d49e3eb44ca02cf5de1e9a8f
illumination-k <illumination.k.27@gmail.com>
2020-03-15 04:25:54 +0900

    edit keybind

---
+++ .zsh/20_keybind.zsh
@@ -0,0 +1,3 @@
+# ctrl-f, ctrl-bキーで単語移動
+bindkey "^F" forward-word
+bindkey "^B" backward-word
--- etc/install.sh
+++ etc/install.sh
@@ -34,8 +34,7 @@ if [ $? -ne 0 ]; then
 fi
 
 # 移動できたらリンクを実行する
-for f in .??*
-do
+for f in .??*; do
     [[ "$f" = ".git" ]] && continue
     [[ "$f" == ".DS_Store" ]] && continue
     [[ "$f" == ".gitignore" ]] && continue
