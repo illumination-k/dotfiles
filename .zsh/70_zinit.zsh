2f3993bc71c1b6e0bfc7496e25a2d8e1bd3793e7
illumination-k <illumination.k.27@gmail.com>
2020-03-15 01:39:35 +0900

    add zinit plug

--- .zsh/70_zinit.zsh
+++ .zsh/70_zinit.zsh
@@ -7,3 +7,6 @@ source ~/.zinit/bin/zinit.zsh
 autoload -Uz _zinit
 (( ${+_comps} )) && _comps[zinit]=_zinit
 
+zinit ice wait'!0'; zinit load zsh-users/zsh-syntax-highlighting
+zinit ice wait'!0'; zinit load zsh-users/zsh-completions
+
